a49ef120265424a4237eeaa52d2fe8db79a7d3ed
tailnw <77036954+tailnw@users.noreply.github.com>
2023-07-19 18:26:42 +0700

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,3 +1,4 @@
+apt-get update && apt-get -y upgrade
 apt-get install -y build-essential nano
 wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.12.tar.gz
 tar xzf 0.8.12.tar.gz
